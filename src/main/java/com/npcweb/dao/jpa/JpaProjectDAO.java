01f3be787c860b0bace288f98f08d7f827dea8ab
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-12-13 18:08:37 +0900

    fix: error

--- src/main/java/com/npcweb/dao/jpa/JpaProjectDAO.java
+++ src/main/java/com/npcweb/dao/jpa/JpaProjectDAO.java
@@ -1,5 +1,15 @@
 package com.npcweb.dao.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
 public class JpaProjectDAO {
+	@PersistenceContext
+	private EntityManager em;
 	// 필요 시 추가
 }
