7db0456f3a06fef555d5126380906fbb9e12fa20
karam Choi <riverchoi2523@gmail.com>
2023-12-13 20:03:31 +0900

    주석 처리

--- src/main/java/com/npcweb/controller/BoardController.java
+++ src/main/java/com/npcweb/controller/BoardController.java
@@ -25,11 +25,12 @@ public class BoardController {
 		List<Post> pList = boardService.getAllPostList(board_id);
 		return pList;
 	}
-	
+	/*
 	//게시글 검색 - 제목
 	@GetMapping("/{board_id}")
 	public List<Post> postList(@PathVariable long board_id,@RequestParam(name="range") long rangeId, @RequestParam(name="searchRange") long searchRange, @RequestParam(name="text") String text ) {
 		List<Post> pList = boardService.getAllPostListByKeyword(board_id, rangeId, searchRange, text);
 		return pList;
 	}
+	*/
 }
