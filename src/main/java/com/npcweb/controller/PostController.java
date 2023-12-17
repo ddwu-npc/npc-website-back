3832abe124d8343b1ec834812ce6d280aed76d4c
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-12-18 01:27:32 +0900

    update: 조회수 계산

--- src/main/java/com/npcweb/controller/PostController.java
+++ src/main/java/com/npcweb/controller/PostController.java
@@ -19,38 +19,36 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.npcweb.dao.jpa.JpaPostDAO;
 import com.npcweb.domain.Post;
 import com.npcweb.security.JWTProvider;
 import com.npcweb.service.PostService;
-import com.npcweb.service.RedisService;
+import com.npcweb.service.ReadCountService;
 import com.npcweb.service.CommentService;
 
 @CrossOrigin(origins = "http://localhost:3000") 
 @RestController
 @RequestMapping("/post")
 public class PostController {
-	@Autowired JpaPostDAO postDao;
 	@Autowired PostService postService;
 	@Autowired CommentService commentService;
-	@Autowired RedisService redisService;
-	@Autowired JWTProvider jwtProvider;
+	@Autowired private JWTProvider jwtProvider;
+	@Autowired private ReadCountService readCountService;
 
 	//read
 	@GetMapping("/{post_id}")
 	public ResponseEntity<Post> readPost(@PathVariable long post_id, @RequestHeader("Authorization") String token) {
+		String jwtToken = token.replace("Bearer ", "").replace("\"", "");
+        long userNo = jwtProvider.getUsernoFromToken(jwtToken);
+		readCountService.getReadCountByUser(post_id, userNo);
 	    try {
 	        Post post = postService.readPost(post_id);
 	        if (post != null) {
-	        	String jwtToken = token.replace("Bearer ", "").replace("\"", "");
-	            long userNo = jwtProvider.getUsernoFromToken(jwtToken);
-	            
-	        	redisService.getReadCountByUser(post_id, userNo);
 	            return ResponseEntity.ok(post);
 	        } else {
 	            return ResponseEntity.notFound().build();
 	        }
 	    } catch (Exception e) {
+	        // Handle the exception and return an error response
 	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	    }
 	}
