2fd1e28653f19244b51092f99e2831b9273dc073
karam Choi <riverchoi2523@gmail.com>
2023-12-17 15:05:30 +0900

    게시글 검색, 게시글 목록

--- src/main/java/com/npcweb/controller/BoardController.java
+++ src/main/java/com/npcweb/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,12 +26,12 @@ public class BoardController {
 		List<Post> pList = boardService.getAllPostList(board_id);
 		return pList;
 	}
-	/*
-	//게시글 검색 - 제목
-	@GetMapping("/{board_id}")
-	public List<Post> postList(@PathVariable long board_id,@RequestParam(name="range") long rangeId, @RequestParam(name="searchRange") long searchRange, @RequestParam(name="text") String text ) {
+	
+	//게시글 검색
+	@PostMapping("/{board_id}/search")
+	public List<Post> postList(@PathVariable long board_id, @RequestParam(name="rangePost") long rangeId, @RequestParam(name="searchRange") long searchRange, @RequestParam(name="text") String text ) {
+		System.out.println("확인용 "+board_id+" "+rangeId+" "+searchRange+" "+text);
 		List<Post> pList = boardService.getAllPostListByKeyword(board_id, rangeId, searchRange, text);
 		return pList;
 	}
-	*/
 }
