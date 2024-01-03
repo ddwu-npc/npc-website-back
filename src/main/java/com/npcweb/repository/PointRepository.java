c55b37a1fb72e797dcac5562257d099c32ef52bb
Fornamin <97093507+Fornamin@users.noreply.github.com>
2024-01-03 17:03:39 +0900

    add: 출석 포인트 부여

--- src/main/java/com/npcweb/repository/PointRepository.java
+++ src/main/java/com/npcweb/repository/PointRepository.java
@@ -1,9 +1,13 @@
 package com.npcweb.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.npcweb.domain.Point;
 
-public interface PointRepository extends CrudRepository<Point, Long>{
+public interface PointRepository extends JpaRepository<Point, Long>{
+
+	List<Point> findByUserno(long userno);
 
 }
